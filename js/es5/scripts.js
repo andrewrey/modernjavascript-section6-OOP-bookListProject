ed9e98827bf4983ff8436e91d424de61959ad2b6
Andrew Reynolds <andrew_reynolds@mac.com>
2020-07-07 14:23:15 -0700

    added form verifcation as well as alert message for success for fail

--- js/es5/scripts.js
+++ js/es5/scripts.js
@@ -8,16 +8,35 @@ function Book(title, author, isbn){
 // UI Constructor
 function UI(){}
 
-// Add book to list
-UI.prototype.addBookToList = function(book){
-  const list = document.getElementById('book-list');
-  
+// Show Alert
+UI.prototype.showAlert = function(message, className){
+  // Create Div
+  const div = document.createElement('div');
+  // Add ClassName
+  div.className = `alert ${className}`;
+  // Add Text
+  div.appendChild(document.createTextNode(message));
+  // Get Parent 
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#book-form');
+  // Insert Alert
+  container.insertBefore(div, form);
+  // Timeout after 3 seconds
+  setTimeout(()=> document.querySelector('.alert').remove(), 3000)
+}
+
+
+
 // Clear Input Fields
 UI.prototype.clearFields = function(){
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
   document.getElementById('isbn').value = '';
 }
+
+// Add book to list
+UI.prototype.addBookToList = function(book){
+  const list = document.getElementById('book-list');
 
   // Create tr element
   const row = document.createElement('tr');
@@ -29,8 +48,8 @@ UI.prototype.clearFields = function(){
     <td><a href="#" class="delete">X</a></td>
   `;
   list.appendChild(row);
-}
 
+}
 
 // Event Listeners
 document.getElementById('book-form').addEventListener('submit', e => {
@@ -42,12 +61,24 @@ document.getElementById('book-form').addEventListener('submit', e => {
   const book = new Book(title, author, isbn);
   // Instantiate UI
   const ui = new UI();
+  console.log(ui);
+  // Validate
+  if(title === '' || author === '' || isbn === ''){
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+    
 
-  // Add Book to List
-  ui.addBookToList(book);
+  } else {
+    // Add Book to List
+    ui.addBookToList(book);
 
-  // Clear input Fields
-  ui.clearFields();
+    // Show success
+    ui.showAlert('Book Added', 'success');
+
+    // Clear input Fields
+    ui.clearFields();
+  }
+
 
   e.preventDefault();
 })
