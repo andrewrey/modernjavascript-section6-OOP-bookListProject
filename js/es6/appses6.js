b4fa344ff480dbd662a5a701fe236ca0f0d5b23a
Andrew Reynolds <andrew_reynolds@mac.com>
2020-07-07 19:22:39 -0700

    added the ability for local storage

--- js/es6/appses6.js
+++ js/es6/appses6.js
@@ -52,6 +52,54 @@ class UI {
 }
 
 
+// Local Storage Class
+
+class Store{
+  static getBooks(){
+    let books;
+    if(localStorage.getItem('books') === null){
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks(){
+    const books = Store.getBooks();
+
+    books.forEach(book=>{
+      const ui = new UI();
+
+      // Add book to UI
+      ui.addBookToList(book);
+    })
+  }
+
+  static addBooks(book){
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbnNumber){
+    const books = Store.getBooks();
+    books.forEach((book,index,array) => {
+      if(book.isbn === isbnNumber){
+        array.splice(index, 1);
+      }
+    })
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', e =>{
+  Store.displayBooks();
+})
+
 // Event Listener to add book
 document.getElementById('book-form').addEventListener('submit', e => {
   // Get Form Values
@@ -73,6 +121,9 @@ document.getElementById('book-form').addEventListener('submit', e => {
     // Add Book to List
     ui.addBookToList(book);
 
+    // Add to LS
+    Store.addBooks(book);
+
     // Show success
     ui.showAlert('Book Added', 'success');
 
@@ -92,9 +143,13 @@ document.getElementById('book-list').addEventListener('click', e => {
 
   // Instantiate UI
   const ui = new UI();
+  
 
   // Delete Book
   ui.deleteBook(e.target);
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show message
   ui.showAlert('Book removed', 'success');
